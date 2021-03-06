3f92ec7ad0a5e09c5da5ba2aad2d6ce265870484
Yaya <861981634@qq.com>
2021-03-06 09:57:03 +0800

    第二次上传

--- demo01.py
+++ demo01.py
@@ -2,26 +2,32 @@
 
 
 # print("哈哈",22,2.33)
-# print("哈哈"+"嘻嘻")    #字符产的拼接
+# print("哈哈"+"嘻嘻")    #字符串的拼接
 # print("哈哈"*10)        
 
 # a = float(input("请输入:"))
 # b = float(input("请输入:"))
 # print("input获取的内容:",a+b)
 
-"""
-练习:通过代码获取两段内容,并且计算他们长度的和
-"""
-
-# a = float(len(input("请输入:")))
-# b = float(len(input("请输入:")))
-# print("两段内容的长度和为:",a+b)
 
 
-# 元组
-# a = (1,2,3,4,"哈哈","哈哈","哈哈","嘻嘻",False,True)
-# # 切片
+# 元组,下表,从0开始
+# a = (1,2,3,4,"哈哈","哈哈","哈哈","嘻嘻",False,True)  在这里修改属于定义
+# print(a[5])           (下标也可以是负数)
+# 切片(批量取值)
 # print(a[0:4]) #左闭右开
+
+
+# print(a.index("哈哈")) (就近原则)
+# print(a.count("哈哈"))
+
+# 二维元组
+# b = (a,"哈哈","嘻嘻")   ("3个值")
+# print(b[0][3])  打印a元组里的4
+
+
+
+
 
 #数组(元组不可修改数组可以)
 # a = [1,2,3,4,"哈哈","哈哈","哈哈","嘻嘻",False,True]
@@ -37,6 +43,7 @@
 # print(a)
 # print(b)
 
+
 """
 下标不要超出范围 = 越界
 所有的方法都是小括号结尾,比如,print(),insert()
@@ -48,59 +55,45 @@
 字典的特点
 1.字典中没有顺序
 2.字典的接口必须是键值对的结构:   key:value
+3.
 """
 
 # a = {
 #     "name":"瓜皮",
 #     "age":12,
-#     "age":122
+#     0:"哈哈"
 # }
 
+
 # #取值
-# print(a["name"])
+# print(a["key"])
+
 # # 新增
-# a["height"] = "183cm"
+# a["key"] = "value"
 # print(a)
+
 # # 修改
-# a["name"] = "李四"
+# a["key"] = "value"
 # print(a)
 
-# b = a.get("name")
+# b = a.get("key")
 # print(b)
-# b = a.pop("name")
+# b = a.pop("key")
 # print(a)
 # print(b)
 
-# a.update(name=11111)
+# a.update(key=value)   key可以不打""
 # print(a)
 
-# print("----------------------------")
+# print("---------get方法取值和直接取值的却别---------")
 
 # # 正常情况下
-# print(a.get("name"))
-# print(a["name"])
+# print(a.get("key"))           输出结果:key
+# print(a["key])                输出结果:key
 
 # # 不正常情况下
-# print(a.get("name11"))
-# print(a["name11"])
-
-"""
-练习:
-获取用户的个人信息,并且存储到字典中
-个人信息包括,name,age,sex
-"""
-
-name = input("请输入您的姓名:")
-age = input("请输入您的年龄:")
-sex = input("请输入您的性别:")
-
-userinfo = {}
-
-userinfo["name"] = name
-userinfo["age"] = age
-userinfo["sex"] = sex
-print(userinfo)
-
+# print(a.get("key1"))          输出结果:None
+# print(a["key"])               输出结果:报错
 
 
 
---
+++ demo02.py
@@ -0,0 +1,58 @@
+#判断
+# age = int(input("请输入您的年龄:"))                 #age = input("请输入您的年龄:") 错误:获取到的是字符串 
+# if age > 60:
+#     print("222")
+# elif age > 30:
+#     print("333")
+# elif age > 20:
+#     print("111")
+# else:
+#     print("00")
+
+
+
+
+#in的用法
+# a = input("请输入:")
+# if a == "":
+#     print("不能为空")
+#     exit()
+
+# if a in "0123456789":
+#     a = int(a)
+# else:
+#     print("请输入正确的年龄:")
+#     exit()
+
+
+
+
+#is的用法
+# a = 1
+# if type(a) is int:
+#     print("他是数字")
+# elif type(a) is str:
+#     print("他是字符串")
+# else:
+#     print("其他")
+
+
+# a = 1
+# while a < 10:
+#     print("哈哈哈",a)
+#     a = a + 2 
+
+
+
+#遍历(把变量中的数据全部打印)
+# a = "你是猪吗"
+# for i in a:
+#     print(i)
+
+
+# range的使用
+# b = list(range(0,50,4)) #range自动生成一个数列
+# print(b)
+
+# end = "" 不会换行,打印""的值
+
